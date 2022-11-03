7283ca7b7d5240a2a5b33f203a588cbd76521805
Anna Maltseva <mr-sbazin@yandex.ru>
2022-11-03 16:43:30 +0300

    "feat:добавлен попап для добавления фото"

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,20 @@
-// popups
-const popup = document.querySelectorAll('.popup');
-const popupEditElement = document.querySelector('.popup_type_edit');
-const popupNewCardElement = document.querySelector('.popup_type_new-card');
-
-// popups close buttons
-const popupEditCloseButtonElement = popupEditElement.querySelector('.popup__close');
-const popupNewCardCloseButtonElement  = popupNewCardElement.querySelector('.popup__close');
-
-//popups open buttons
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const profileEditButtonElement = document.querySelector('.profile__edit-button');
-const profileAddButtonElement = document.querySelector('.profile__add-button');
-
-//like button
-const likeButtonElement = document.querySelectorAll('.card__like');
-
 let profileName = document.querySelector('.profile__title');
 let profileAbout = document.querySelector('.profile__subtitle');
 let popupFormElement = document.querySelector('.popup__form');
 let inputName = popupFormElement.querySelector('.popup__field_input_name');
 let inputAbout = popupFormElement.querySelector('.popup__field_input_about');
 let popupSubmitButtonElement = document.querySelector('.popup__submit-button');
-let inputPlace = popupFormElement.querySelector('.popup__field_input_place');
-let inputLink = popupFormElement.querySelector('.popup__field_input_link');
 
-
-const openPopup = function(popupElement) {
+const openPopup = function() {
   popupElement.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputAbout.value = profileAbout.textContent;
 }
 
-const closePopup = function(popupElement) {
+const closePopup = function() {
   popupElement.classList.remove('popup_opened');
 }
 
@@ -45,33 +31,10 @@ let addProfileInfo = function(evt) {
   profileAbout.textContent = inputAbout.value;
   closePopup();
 }
-  
-//event listeners
-profileEditButtonElement.addEventListener('click', function() {
-  openPopup(popupEditElement);
-});
 
-profileAddButtonElement.addEventListener('click', function() {
-  openPopup(popupNewCardElement);
-});
+profileEditButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
 
-popupEditCloseButtonElement.addEventListener('click', function() {
-  closePopup(popupEditElement);
-});
-
-popupNewCardCloseButtonElement.addEventListener('click', function() {
-  closePopup(popupNewCardElement);
-});
-
-// popupEditElement.addEventListener('click', closePopupByClickOnOverlay);
-
-//popupNewCardElement.addEventListener('click', closePopupByClickOnOverlay);
-
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
 
 popupFormElement.addEventListener('submit', addProfileInfo);
-
-likeButtonElement.forEach(function(element) {
-  element.addEventListener('click', function(evt) {
-    evt.target.classList.toggle('card__like_active');
-  })
-});
