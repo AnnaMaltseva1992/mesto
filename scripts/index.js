04e31b98389cb35ed7dd212f81b9fc5e20f47c69
Anna Maltseva <mr-sbazin@yandex.ru>
2022-11-04 17:16:57 +0300

    feat: добавлены попапы

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,50 @@
-const popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+// popups
+const popupEditElement = document.querySelector('.popup_type_edit');
+const popupNewCardElement = document.querySelector('.popup_type_new-card');
+const popupImageElement = document.querySelector('popup_type_image');
+
+// popups close buttons
+const popupEditCloseButtonElement = popupEditElement.querySelector('.popup__close');
+const popupNewCardCloseButtonElement  = popupNewCardElement.querySelector('.popup__close');
+
+//popups open buttons
 const profileEditButtonElement = document.querySelector('.profile__edit-button');
+const profileAddButtonElement = document.querySelector('.profile__add-button');
+const cardPhotoElement = document.querySelectorAll('.card__photo');
+
+//like button
+const likeButtonElement = document.querySelectorAll('.card__like');
+
 let profileName = document.querySelector('.profile__title');
 let profileAbout = document.querySelector('.profile__subtitle');
 let popupFormElement = document.querySelector('.popup__form');
 let inputName = popupFormElement.querySelector('.popup__field_input_name');
 let inputAbout = popupFormElement.querySelector('.popup__field_input_about');
 let popupSubmitButtonElement = document.querySelector('.popup__submit-button');
+let inputPlace = popupFormElement.querySelector('.popup__field_input_place');
+let inputLink = popupFormElement.querySelector('.popup__field_input_link');
 
-const openPopup = function() {
+
+const openPopup = function(popupElement) {
   popupElement.classList.add('popup_opened');
-  inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;
 }
 
-const closePopup = function() {
+const closePopup = function(popupElement) {
   popupElement.classList.remove('popup_opened');
 }
 
-const closePopupByClickOnOverlay = function(event) {
+const closePopupEditElementByClickOnOverlay = function(event) {
   if (event.target !== event.currentTarget) {
     return;
   }
-  closePopup();
+  closePopup(popupEditElement);
+}
+
+const closePopupNewCardElementByClickOnOverlay = function(event) {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+  closePopup(popupNewCardElement);
 }
 
 let addProfileInfo = function(evt) {
@@ -31,10 +53,40 @@ let addProfileInfo = function(evt) {
   profileAbout.textContent = inputAbout.value;
   closePopup();
 }
+  
+let likeButtonActive = function(evt) {
+  evt.target.classList.toggle('card__like_active');
+}
 
-profileEditButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
+//event listeners
+profileEditButtonElement.addEventListener('click', function() {
+  openPopup(popupEditElement);
+});
 
-popupElement.addEventListener('click', closePopupByClickOnOverlay);
+profileAddButtonElement.addEventListener('click', function() {
+  openPopup(popupNewCardElement);
+});
+
+cardPhotoElement.forEach(function(element) {
+  element.addEventListener('click', function() {
+    openPopup(popupImageElement);
+  })
+});
+
+popupEditCloseButtonElement.addEventListener('click', function() {
+  closePopup(popupEditElement);
+});
+
+popupNewCardCloseButtonElement.addEventListener('click', function() {
+  closePopup(popupNewCardElement);
+});
+
+popupEditElement.addEventListener('click', closePopupEditElementByClickOnOverlay);
+
+popupNewCardElement.addEventListener('click', closePopupNewCardElementByClickOnOverlay);
 
 popupFormElement.addEventListener('submit', addProfileInfo);
+
+likeButtonElement.forEach(function(element) {
+  element.addEventListener('click', likeButtonActive);
+});
